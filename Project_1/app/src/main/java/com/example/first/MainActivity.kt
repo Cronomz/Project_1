7f9b6bdecfce2a2ffb39676cbcae77f21fb29ecd
Kris Natanael Prawiro <kprawiro376@berkeley.edu>
2021-05-20 07:44:44 -0700

    version 0.1

--- Project_1/app/src/main/java/com/example/first/MainActivity.kt
+++ Project_1/app/src/main/java/com/example/first/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var projectAdapter: ProjectAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- Project_1/app/src/main/java/com/example/first/MainFragment.kt
+++ Project_1/app/src/main/java/com/example/first/MainFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.first.databinding.ProjectPageBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainFragment: Fragment() {
+
+    private lateinit var projectAdapter: ProjectAdapter
+
     private var _binding: ProjectPageBinding? = null
 
     private val binding get() = _binding!!
